6c46fd05011d2600e1fb358868715faaa8e937a1
BuschAmeise <shawnlange@web.de>
2024-06-04 14:03:03 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 function setup() {
   createCanvas(400, 400);
+  y = width/2;
+  velY=0;
+  posY=0;
+}
+
+function physics(y) {
+  accY = random(-1,1);
+  velY += accY;
+  posY = y+velY;
+  return (posY)
+}
+
+function bounce(y) {
+  if (y > width) {
+    y = width/2
+  }
 }
 
 function draw() {
   background(220);
-  ellipse(200, 200, 50, 50);
+  ellipse(200, physics(y), 50, 50);
 }
